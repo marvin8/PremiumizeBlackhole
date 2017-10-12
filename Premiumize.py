e1e979f2e59802efbb8bd4c2313739bb8c51e896
Mark S Burgunder <mark@budafoods.com.au>
2017-10-12 18:07:32 +1000

    Added checking of download status and print out of direct download link with approximate size. New dependencie of humanize (sudo pip install humanize)

--- Premiumize.py
+++ Premiumize.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 import glob
+import humanize
 import json
 import requests
 import os
@@ -23,17 +24,26 @@ default_config["premiumize"] = {}
 default_config["premiumize"]["customer_id"] = "<change this to your premiumize customer_id>"
 default_config["premiumize"]["pin"] = "<change this to your premiumize PIN>"
 
+# Read config file
 try:
   with open('config', 'r') as config_file:
     config=json.load(config_file)
 
+# If config file can't be read create a new config file with default values
 except IOError:
   with open('config', 'w') as config_file:
     json.dump(default_config, config_file, sort_keys = True, indent = 4)
     config = default_config
     print "Default config created. Please check values in 'config' file"
 
+
+# Load file with Premiumize ID cache
 id_cache = {}
+with open(config["directories"]["in_progress_hash_cache"] + 'premiumize_id_cache', 'r') as id_cache_file:
+  id_cache=json.load(id_cache_file)
+  print("ID Cache loaded:")
+  print(json.dumps(id_cache, sort_keys = True, indent = 4))
+
 
 print('')
 print('Processing .torrent files')
@@ -44,6 +54,7 @@ for filename in glob.glob(os.path.join(config["directories"]["torrents"], config
   response=request.json()
   id_cache[response["id"]] =  response["name"]
   print(response["status"] + ": " + response["name"])
+
 
 print('')
 print('Processing .magnet files')
@@ -56,6 +67,7 @@ for filename in glob.glob(os.path.join(config["directories"]["magnets"], config[
     id_cache[response["id"]] =  response["name"]
     print(response["status"] + ": " + response["name"])
 
+
 print('')
 print('Processing .nzb files')
 for filename in glob.glob(os.path.join(config["directories"]["nzbs"], config["file_types"]["nzbs"])):
@@ -67,6 +79,30 @@ for filename in glob.glob(os.path.join(config["directories"]["nzbs"], config["fi
 #  print(request.text)
 
 
+# Check hashes
+print("")
+print("Check download status")
+hashes_string = ""
+for cache_id, name in id_cache.iteritems():
+  hashes_string = hashes_string + "&hashes[]=" + cache_id
+
+response = requests.get("https://www.premiumize.me/api/torrent/checkhashes?customer_id=" + config["premiumize"]["customer_id"] + "&pin=" + config["premiumize"]["pin"] + hashes_string)
+for hash_id, array in response.json()["hashes"].iteritems():
+  print(id_cache[hash_id] + ": " + array["status"])
+
+ 
+# Check status of each download in Premiumize ID cache
+for cache_id, name in id_cache.iteritems():
+  request_pars = {"customer_id": config["premiumize"]["customer_id"], "pin": config["premiumize"]["pin"], "hash": cache_id}
+  response = requests.post("https://www.premiumize.me/api/torrent/browse", data=request_pars)
+  print("")
+  print("Download info for: " + name)
+  print("Size: " + humanize.naturalsize(response.json()["size"]))
+  print(response.json()["zip"])
+#  print(json.dumps(response.json(), sort_keys = True, indent = 4))
+
+
+# Write Premiumize ID cache to file for persistence
 with open(config["directories"]["in_progress_hash_cache"] + 'premiumize_id_cache', 'w') as id_cache_file:
   json.dump(id_cache, id_cache_file, sort_keys = True, indent = 4)
 
