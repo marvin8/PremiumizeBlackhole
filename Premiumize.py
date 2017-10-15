fbb36de977fa82f34e1ce12bd2b951ae82a80f65
Mark S Burgunder <mark@budafoods.com.au>
2017-10-15 15:56:20 +1000

    Refactor to make adding a file to Premiumize download manager a function

--- Premiumize.py
+++ Premiumize.py
@@ -48,50 +48,44 @@ except:
   id_cache={}
 
 
+def Add2DownloadManager(fileName, fileType):
+  _response = {}
+  if fileType == "magnet":
+    with open(fileName, 'r') as _magnet_file:
+      _magnet_link = _magnet_file.read()
+      _request_pars = {"customer_id": config["premiumize"]["customer_id"], "pin": config["premiumize"]["pin"], "type": "torrent", "src": _magnet_link}
+      _response = requests.post("https://www.premiumize.me/api/transfer/create", data=_request_pars)
+  else:
+    _request_pars = {"customer_id": config["premiumize"]["customer_id"], "pin": config["premiumize"]["pin"], "type": fileType}
+    _filePar = {"src": open(fileName, "rb")}
+    _response = requests.post("https://www.premiumize.me/api/transfer/create", data=_request_pars, files=_filePar)
+
+  _responseArr = _response.json()
+  id_cache[_responseArr["id"]] = _responseArr["name"]
+  _addMsg = " "
+  if _responseArr["status"] == "success":
+    os.remove(fileName)
+    _addMsg = " - " + fileType + " file removed"
+
+  print("Premiumize - add to download manager: " + _responseArr["status"] + _addMsg +" - " + _responseArr["name"])
+  
+
 print('')
 print('Processing .torrent files')
 for filename in glob.glob(os.path.join(config["directories"]["torrents"], config["file_types"]["torrents"])):
-  torrent = {"src": open(filename, "rb")}
-  request_pars = {"customer_id": config["premiumize"]["customer_id"], "pin": config["premiumize"]["pin"], "type": "torrent"}
-  request = requests.post("https://www.premiumize.me/api/transfer/create", data=request_pars, files=torrent)
-  response=request.json()
-  id_cache[response["id"]] =  response["name"]
-  if response["status"] == "success":
-    os.remove(filename)
-    print(response["status"] + ": " + response["name"] + " - .torrent file removed!")
-  else:
-    print(response["status"] + ": " + response["name"])
+  Add2DownloadManager(filename, "torrent")
 
 
 print('')
 print('Processing .magnet files')
 for filename in glob.glob(os.path.join(config["directories"]["magnets"], config["file_types"]["magnets"])):
-  with open(filename, 'r') as magnet_file:
-    magnet_link=magnet_file.read()
-    request_pars = {"customer_id": config["premiumize"]["customer_id"], "pin": config["premiumize"]["pin"], "type": "torrent", "src": magnet_link}
-    request = requests.post("https://www.premiumize.me/api/transfer/create", data=request_pars)
-    response=request.json()
-    id_cache[response["id"]] =  response["name"]
-    if response["status"] == "success":
-      os.remove(filename)
-      print(response["status"] + ": " + response["name"] + " - .magent file removed!")
-    else:
-      print(response["status"] + ": " + response["name"])
+  Add2DownloadManager(filename, "magnet")
 
 
 print('')
 print('Processing .nzb files')
 for filename in glob.glob(os.path.join(config["directories"]["nzbs"], config["file_types"]["nzbs"])):
-  nzb = {"src": open(filename, "rb")}
-  request_pars = {"customer_id": config["premiumize"]["customer_id"], "pin": config["premiumize"]["pin"], "type": "nzb"}
-  request = requests.post("https://www.premiumize.me/api/transfer/create", data=request_pars, files=nzb)
-  response=request.json()
-  id_cache[response["id"]] =  response["name"]
-  if response["status"] == "success":
-    os.remove(filename)
-    print(response["status"] + ": " + response["name"] + " - .nzb file removed!")
-  else:
-    print(response["status"] + ": " + response["name"])
+  Add2DownloadManager(filename, "nzb")
 
 
 # Check hashes
